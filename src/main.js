5917d6cb3ffb01dbe73d89a11ea3a6060cf1c83d
swakswak <hschoi0702@gmail.com>
2022-06-21 21:39:16 +0900

    vuex 설정

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import store from "@/store";
+
+const app = createApp(App);
+app.mount('#app');
+app.use(store);
