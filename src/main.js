39c143db84304960ccb6e6c031db8f6ec72926a8
swakswak <hschoi0702@gmail.com>
2022-06-27 00:31:08 +0900

    주석 제거

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from "@/store";
-// import vueMoment from "vue-moment"
 
 const app = createApp(App);
 app.use(store);
-// app.use(vueMoment);
 app.mount('#app');
